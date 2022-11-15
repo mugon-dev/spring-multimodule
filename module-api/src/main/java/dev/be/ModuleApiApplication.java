8d72bf1f741967922cc54c7568b2e479dfddd19b
Conny <jmg41490400@gmail.com>
2022-11-15 10:30:46 +0900

    Bean을 사용한 모듈간 의존성 테스트 - scan path 변경으로 적용

--- module-api/src/main/java/dev/be/ModuleApiApplication.java
+++ module-api/src/main/java/dev/be/ModuleApiApplication.java
@@ -1,5 +1,6 @@
-package dev.be.moduleapi;
-
+package dev.be;
+// component scan의 해당 package의 하위만 스캔해서 bean으로 등록
+// scan범위를 common 모듈과 맞춰주기 위해 package path를 변경
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
+++ module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -1,17 +1,23 @@
 package dev.be.moduleapi.service;
 
 import dev.be.modulecommon.enums.CodeEnum;
+import dev.be.modulecommon.service.CommonDemoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class DemoService {
 
-    public String save(){
+    private final CommonDemoService commonDemoService;
+
+    public String save() {
         System.out.println(CodeEnum.SUCCESS.getCode());
+        System.out.println(commonDemoService.commonService());
         return "save";
     }
 
-    public String find(){
+    public String find() {
         return "find";
     }
 }
