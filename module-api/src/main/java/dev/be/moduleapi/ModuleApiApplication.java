0a0152acbf5ce560f67bc2dcd7a20e89da7bfcfe
Conny <jmg41490400@gmail.com>
2022-11-15 10:34:35 +0900

    Bean을 사용한 모듈간 의존성 테스트 - scan할 모듈을 명시

--- module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
@@ -1,10 +1,12 @@
-package dev.be;
+package dev.be.moduleapi;
 // component scan의 해당 package의 하위만 스캔해서 bean으로 등록
 // scan범위를 common 모듈과 맞춰주기 위해 package path를 변경
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+// package path 변경 대신 scan할 base package 명시
+@SpringBootApplication(
+	scanBasePackages = {"dev.be.moduleapi","dev.be.modulecommon"}
+)
 public class ModuleApiApplication {
 
 	public static void main(String[] args) {
